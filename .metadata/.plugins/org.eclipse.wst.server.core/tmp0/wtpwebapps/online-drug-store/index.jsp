8f7217a7d557980f1b5d89eb03d3db6bdc76abeb
GeethOnion <geethmaka@gmail.com>
2021-05-02 21:06:42 +0530

    add gitignore

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/online-drug-store/index.jsp
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/online-drug-store/index.jsp
@@ -2,14 +2,13 @@
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-<h1>Home Page</h1>
-<form action="home" method="get">
-<input type="submit"/>
-</form>
-</body>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Insert title here</title>
+	</head>
+	<body>
+		<!--<div style="position:absolute;top:0;left:0;height:10%;width:100%;background:red;">d</div>-->
+		<div style="height:80%;width:100%;background:blue;">test</div>
+		<div style="position:absolute;bottom:0;left:0;height:10%;width:100%;background:red;">d</div>
+	</body>
 </html>
--- online-drug-store/src/main/java/home/Home.java
+++ online-drug-store/src/main/java/home/Home.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.*;
+import java.sql.*;
 //import javax.servlet.ServletException;
 //import javax.servlet.annotation.WebServlet;
 //import javax.servlet.http.HttpServlet;
@@ -39,6 +40,35 @@ public class Home extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+
+	      // variables
+	      final String url = "jdbc:http://localhost/phpmyadmin/";
+	      final String user = "root";
+	      final String password = "";
+
+	      // establish the connection
+	      Connection con;
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		      // display status message
+		      if (con == null) {
+		    	  response.getWriter().append("null");
+		         return;
+		      } else
+		    	  response.getWriter().append("Congratulations," + 
+		              " JDBC connection is established successfully.\n");
+
+		      // close JDBC connection
+		      con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			response.getWriter().append(e.toString());
+		}
+
+
+
+
 	}
 
 }
